3bb8d2793674e3e3a353b021cbaa13f6e24830b4
silentshaikh <sheikhabdulmoiz283@gmail.com>
2023-11-18 22:20:10 +0500

    update error

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import {useNavigate} from 'react-router-dom';
 function Error() {
+  const portNavig = useNavigate();
   return (
-    <div>
-      Error
-    </div>
+   <section className='flex justify-center items-center flex-col h-screen'>
+    <h1 className='text-4xl text-teal-300'>404 Error</h1>
+    <button className='flex justify-center items-center px-7 py-3 border text-lg bg-teal-300 text-white rounded-full mt-3' onClick={() => portNavig('/')}>Go Back</button>
+   </section>
   )
 }
 
