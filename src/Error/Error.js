3e299cbd719a3fb2fddbf51dfbbf90e97d2ed8a2
silentshaikh <sheikhabdulmoiz283@gmail.com>
2023-11-20 15:08:30 +0500

    update heading

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -4,8 +4,8 @@ function Error() {
   const portNavig = useNavigate();
   return (
    <section className='flex justify-center items-center flex-col h-screen pl-[250px] max-[1070px]:pl-0'>
-    <h2 className='text-4xl text-teal-300'>404</h2>
-    <h3 className='text-4xl text-teal-400'>Error</h3>
+    <h2 className='text-3xl text-teal-300 pb-2'>404</h2>
+    <h3 className='text-4xl text-teal-400 pb-2'>Error</h3>
     <button className='flex justify-center items-center px-7 py-3 border text-lg bg-teal-300 text-white rounded-full mt-3' onClick={() => portNavig('/')}>Go Back</button>
    </section>
   )
